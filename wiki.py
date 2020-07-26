e1922f68de68f8ab9953cb3a03cbb3485cb5842f
SWERecker <SWERecker@users.noreply.github.com>
2020-07-27 00:37:18 +0800

    修复 wiki数据统计+历史数据记录

--- wiki.py
+++ wiki.py
@@ -1,8 +1,8 @@
-import sqlite3
 import json
 import config
 import time
 import os
+import redis
 file_path = 'C:\\wwwroot\\moca.swenetech.xyz'
 new_data = {'updateTime': str(time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time())))}
 
@@ -15,23 +15,22 @@ for names in names_list:
 new_data['peopleCount'] = len(names_list)
 new_data['picCount'] = sum(file_count_list)
 
-conn = sqlite3.connect('mocabot.sqlite3')
-conn_cursor = conn.cursor()
-cursor = conn_cursor.execute('SELECT CONTENT FROM COUNT')
-final_data = []
-for g in list(cursor):
-    for name in g:
-        final_data.append(name)
-# count_dict_list = json.loads(list(cursor)[0][0])
-conn.close()
-# return count_dict_list
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0, decode_responses=True)
+r = redis.Redis(connection_pool=pool)
 count = 0
-for group_data in range(len(final_data)):
-    group_dict = json.loads(final_data[group_data])
-    for people in group_dict:
-        count += group_dict[people]
+g_count = {}
+data = r.hgetall("COUNT")
+for d in data:
+    dict_data = json.loads(data[d])
+    g_count[str(d)] = 0
+    for n in dict_data:
+        g_count[str(d)] += dict_data[n]
+        count += dict_data[n]
 
 new_data['rCount'] = count
 
 with open(file_path + '\\data.json','w',encoding='utf-8')as data_file:
+    data_file.write(json.dumps(new_data, ensure_ascii=False))
+with open(file_path + '\\data_history.json','a',encoding='utf-8')as data_file:
+    data_file.write("\n")
     data_file.write(json.dumps(new_data, ensure_ascii=False))
